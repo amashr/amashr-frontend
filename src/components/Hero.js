06032f194de768265a6606ab67ef53792f72bb3e
Frugence Fidel <frugencefidel@gmail.com>
2019-02-02 20:01:33 +0300

    Style the bottom gradient of Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -23,6 +23,32 @@ const Hero = () => (
     <div className="Hero__img">
       <img src={interactionImg} alt="Interaction" />
     </div>
+    <svg
+      className="Hero__bottomGradient"
+      width="1440"
+      height="279"
+      viewBox="0 0 1440 279"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 60.4332C660 341.448 871 52.3755 1440 0V279H0V60.4332Z"
+        fill="url(#paint0_linear)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear"
+          x1="720"
+          y1="-6.50535e-06"
+          x2="720"
+          y2="279"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.491713" stopColor="#0552B5" />
+          <stop offset="1" stopColor="#47A3F3" />
+        </linearGradient>
+      </defs>
+    </svg>
   </main>
 );
 
