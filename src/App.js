761892d6ba298c6e96b5e6bd76731269691c1c39
Ally Maumba <amaumba1@gmail.com>
2019-02-03 09:57:16 -0600

    demo pipeline

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            AMASHR Under construction 
           </a>
         </header>
       </div>
