0a4e56b3f84c900b359314b95f3a13e26a3b67f6
Frugence Fidel <frugencefidel@gmail.com>
2019-02-11 23:12:48 +0300

    Add content to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from './Button';
 
 const Header = () => (
-  <header>
-    <p>Header</p>
-  </header>
+  <div className="Header">
+    <div className="Header__brand">
+      <Link to="/">AMAS HR</Link>
+    </div>
+    <nav className="Header__nav">
+      <Link to="/platform">Our Platform</Link>
+      <Link to="/about">Why AMAS HR</Link>
+      <Link to="/packages">Packages</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/resources">Resources</Link>
+      <Link to="/signin">Sign In</Link>
+      <Button path="#" desc="Get Started" cssClass="Btn" />
+    </nav>
+  </div>
 );
 
 export default Header;
