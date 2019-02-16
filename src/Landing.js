56704d82780225b761afc3dec4dc9b4bbdf11f88
Frugence Fidel <frugencefidel@gmail.com>
2019-02-16 15:49:05 +0300

    Added Experience component

--- src/Landing.js
+++ src/Landing.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Partnering from './components/Partnering';
+import Experience from './components/Experience';
 
 class Landing extends Component {
   render() {
@@ -10,6 +11,7 @@ class Landing extends Component {
         <Header />
         <Hero />
         <Partnering />
+        <Experience />
       </Fragment>
     );
   }
---
+++ src/components/Experience.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Experience = () => (
+  <div className="Experience">
+  <p>Experience goes here ...</p>
+  </div>
+);
+
+export default Experience;
